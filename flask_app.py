c1bc7fa3332de995e6ec10f0a5d74facaadb57bd
Carlo <10991476+carlo89p@users.noreply.github.com>
2026-06-04 09:04:55 -0400

    Added auto redirect on load

--- flask_app.py
+++ flask_app.py
@@ -3,4 +3,14 @@ from application import create_app
 app = create_app('ProductionConfig')
 
 if __name__ == '__main__':
+    app.run()from application import create_app
+from flask import redirect
+
+app = create_app('ProductionConfig')
+
+@app.route('/')
+def index():
+    return redirect('/api/docs')
+
+if __name__ == '__main__':
     app.run()
